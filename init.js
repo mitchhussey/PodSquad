330aba7bcc23885c59cf3788ba23aec970e0dac4
Mitch Hussey <mitchhussey@gmail.com>
2018-09-18 16:55:16 -0500

    Started to add the search functionality and added js for favoriting people

--- init.js
+++ init.js
@@ -1,3 +1,40 @@
 $(document).ready(function(){
     $('.fixed-action-btn').floatingActionButton();
+    var y = document.getElementById("favorite");
+    y.style.display = "none";
   });
+
+function favorite() {
+    var x = document.getElementById("notFavorite");
+    var y = document.getElementById("favorite");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+    
+}
+
+function searchName() {
+    // Declare variables 
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      } 
+    }
+  }
